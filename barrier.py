80e17f6f0e90393008ed7802d3f118a98c0419ed
egor56ru <55253086+egor56ru@users.noreply.github.com>
2019-12-16 23:05:05 +0300

    Add files via upload

--- barrier.py
+++ barrier.py
@@ -6,29 +6,48 @@ Created on Fri Nov 22 16:29:09 2019
 """
 
 import pygame
+'''
+библиотека для создания игр
+'''
 from gameobject import Object
-from pygame.time import Clock
+'''
+gameobject представляет собой визуальный объект,
+знающий о том, как себя рендерить, сохранять свои границы и перемещаться
+'''
 
 class Barrier(Object):
-    def __init__(self, image='block', pos=[0, 0], size=None, countdown=None):
+    '''описание еды
+        size:   размер, берется из дэфолтного файла, если не не задан другой файл 
+        pos:    позиция
+        drawn:  отображен ли уже объект
+        timer:  время жизни
+        clock:  функция из pygame.time, позволяющая отслеживать время жизни
+        img:    изображение объекта
+
+    '''
+    def __init__(self, image, pos=[0, 0], size=None):
+        '''
+        задаем начальные значения
+        '''
         super().__init__(type='barrier')
         self.img = pygame.image.load(f'images/{image}.png')
         self.size = size if size else self.img.get_size()
         self.pos = pos
         self.drawn = False
-        self.coundown = -countdown if countdown else None
-        self.clock = Clock()
         
     def __call__(self, control):
+        '''
+        отрисовка барьера
+        '''
         if not self.drawn:
             control.window.blit(self.img, self.pos)
             self.drawn = True
-        if not self.coundown is None:
-            self.coundown += self.clock.tick()
-            if self.coundown >= 0:
-                control -= self
     
     def destruct(self, control):
+        '''
+        Обновление картинки на экране,
+        вместо перерисовки всего изображения изменяем только одну зону
+        '''
         if self.drawn:
             pygame.draw.rect(control.window, control.bgcolor,
                              pygame.Rect(
--- food.py
+++ food.py
@@ -5,11 +5,33 @@ Created on Sat Nov 23 12:11:20 2019
 @author: egor56ru
 """
 import pygame
+'''
+библиотека для создания игр
+'''
 from gameobject import Object
+'''
+gameobject представляет собой визуальный объект,
+знающий о том, как себя рендерить, сохранять свои границы и перемещаться
+'''
 from pygame.time import Clock
+'''
+ модуль, который используется для взаимодействия с игровым времени и его управлением
+'''
 
 class Food(Object):
+    '''описание еды
+        size:   размер, берется из дэфолтного файла, если не не задан другой файл 
+        pos:    позиция
+        drawn:  отображен ли уже объект
+        timer:  время жизни
+        clock:  функция из pygame.time, позволяющая отслеживать время жизни
+        img:    изображение объекта
+
+    '''
     def __init__(self, image='food', pos=[0, 0], size=None):
+        '''
+        задаем начальные значения
+        '''
         super().__init__(type='food')
         self.img = pygame.image.load(f'images/{image}.png')
         self.size = size if size else self.img.get_size()
@@ -19,6 +41,9 @@ class Food(Object):
         self.clock = Clock()
         
     def __call__(self, control):
+        '''
+        отрисовка еды, с учетом времени её жизни
+        '''
         if not self.drawn:
             control.window.blit(self.img, self.pos)
             self.drawn = True
@@ -27,6 +52,10 @@ class Food(Object):
         self.timer += self.clock.tick()
     
     def destruct(self, control):
+        '''
+        Обновление картинки на экране,
+        вместо перерисовки всего изображения измеянем только одну зону
+        '''
         if self.drawn:
             pygame.draw.rect(control.window, control.bgcolor,
                              pygame.Rect(
