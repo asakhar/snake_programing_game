5b7c4c9d93ac6303ff1d3b45c1ef53d3868cb58a
Dimasbelousov <58813964+Dimasbelousov@users.noreply.github.com>
2019-12-20 05:14:28 +0300

    Add files via upload

--- guinterface.py
+++ guinterface.py
@@ -14,9 +14,9 @@ def ColorChooserButton_v2(button_text, target=(None, None), image_filename=None,
                        image_subsample=None, tooltip=None, border_width=None, size=(None, None), auto_size_button=None,
                        button_color=None, disabled=False, font=None, bind_return_key=False, focus=False, pad=None,
                        key=None, metadata=None, enable_events=True):
-    """Function that returns the button in the window of the pregame interface with all options included, the 
-    function itself is inside the main class "GUI"
-    """   
+ """Function that returns the button in the window of the pregame interface with all options included, the 
+function itself is inside the main class "GUI"
+"""   
     return sg.Button(button_text=button_text, button_type=sg.BUTTON_TYPE_COLOR_CHOOSER, target=target,
                   image_filename=image_filename, image_data=image_data, image_size=image_size,
                   image_subsample=image_subsample, border_width=border_width, tooltip=tooltip, size=size,
@@ -24,14 +24,14 @@ def ColorChooserButton_v2(button_text, target=(None, None), image_filename=None,
                   bind_return_key=bind_return_key, focus=focus, pad=pad, key=key, metadata=metadata, enable_events=enable_events)
 
 def create_from_template(color):
-    """ Function that creates an image for the "Snake" from the colour that is chosen by the player. In case the colour is already in 
-    template, the function does not create the image but uses the template, otherwise it creates a new one and saves it in the folder "Images".
-    The process of making a new coloured image consists of:
-    -creation of a new image(RGBA - format with 4x8-bit pixels, size-10x10, colour)
-    -creation of file with a directory and transmission of the pixels data(six hexademical numbers, what are inside tuple in var "clr", from the
-    ingame menu "Choosing the colour")
-    -saving a new image with a new colour 
-    """    
+""" Function that creates an image for the "Snake" from the colour that is chosen by the player. In case the colour is already in 
+template, the function does not create the image but uses the template, otherwise it creates a new one and saves it in the folder "Images".
+The process of making a new coloured image consists of:
+-creation of a new image(RGBA - format with 4x8-bit pixels, size-10x10, colour)
+-creation of file with a directory and transmission of the pixels data(six hexademical numbers, what are inside tuple in var "clr", from the
+ingame menu "Choosing the colour")
+-saving a new image with a new colour 
+"""
     new_img = Image.new("RGBA", (10, 10), "white")
     new_pa = new_img.load()
     img = Image.open('images/template.png')
@@ -47,19 +47,21 @@ def create_from_template(color):
 
 class GUI:
     def __init__(self, pos=(0, 0)):
-        """Class is responsible for the pregame window interface, it appears each time the player runs the game.
-        Method __init__ launches a constructor inside the class, as it is commonly used in programming,
-        -self.layout consists of the main body of the window, sg.Text displays the in-script written text,
-        sg.Button displays the button in the window, sg.Filebrowse is responsible for finding scripts in the
-        directory of the programme files or outside directory, sg.InputText is responsible for  
-        colorchooserbutton is responsible for choosing the
-        colour of each snake
-        -self.window displays the window with the intop message 
-        """
+    """Class is responsible for the pregame window interface, it appears each time the player runs the game.
+    Method __init__ launches a constructor inside the class, as it is commonly used in programming,
+    -self.layout consists of the main body of the window, sg.Text displays the in-script written text,
+    sg.Button displays the button in the window, sg.Filebrowse is responsible for finding scripts in the
+    directory of the programme files or outside directory, sg.InputText is responsible for  
+    colorchooserbutton is responsible for choosing the
+    colour of each snake
+    -self.window displays the window with the intop message 
+    """
         self.layout = [
                 [sg.Text(f'Player{i}', key=f'label{i}'), 
-                 sg.FileBrowse('Choose script', key=f'script{i}', 
-                               enable_events=True, target=f'script{i}'), 
+                 sg.FileBrowse('Choose script', key=f'script{i}b', 
+                               enable_events=True, target=f'script{i}'),
+                 sg.InputText(f'template.py', 
+                              key=f'script{i}', visible=False, enable_events=True),
                  sg.InputText(
                 f'#{"ff" if not i else "00"}{"ff" if i == 2 else "00"}{"ff" if i == 1 else "00"}', 
                               key=f'color{i}', visible=False, enable_events=True),
@@ -75,16 +77,20 @@ class GUI:
                                 layout=self.layout, location=pos)
     
     def __call__(self, control):
-        """Method is called each time the player clicks on the body of pregame window and calls different
-        objects as functions
-        """
+    """Method is called each time the player clicks on the body of pregame window and calls different
+    objects as functions
+    """
         event, values = self.window(timeout=1)
-        if event == 'Exit':
+        if event in ('Exit', 'Quit', None):
             self.window.close()
+            control.run = False
             return
         if event.startswith('color'):
+            if values[event] == 'None':
+                values[event] = '#000000'
+                self.window.FindElement(event).update(values[event])
             self.window.FindElement(f'b{event}').update(button_color=('white', 
-                                   values[event]))
+                                       values[event]))
         elif event.startswith('disable'):
             index = event.replace('disable', '')
             try:
--- snakescript1.py
+++ snakescript1.py
@@ -35,7 +35,7 @@ class Behavior:
         i = 0
         while i<len(alg):
             if alg[i]<4:
-                i += ((data[alg[i]][1] =='self')or(data[alg[i]][1] =='barrier'))*((data[alg[i]][2] <= 10) + 1) + (data[alg[i]][1] == 'food')*3
+                i += ((data[alg[i]][1] =='self')or(data[alg[i]][1] =='barrier'))*(data[alg[i]][2] <= 10) + 1 + (data[alg[i]][1] == 'food')*2
             elif alg[i]<8:
                 cur = self.dir_order[alg[i]-4]
                 break
@@ -48,26 +48,16 @@ class Behavior:
     def run(self, kwargs: KeyWordArguments):
         data = kwargs.data
         cur = kwargs.direction
-    #    alg = kwargs['alg'] if 'alg' in kwargs else None
         deaths = kwargs.deaths
-        #health = kwargs['health']
         cycles = kwargs.cycles
-        #print(len(kwargs))
-        #print(population)
-    #    print(prevdeaths, deaths)
         if not self.population:
-    #        alg = list(map(int, '0 10 9 4 1 10 9 5 2 10 9 6 3 10 9 7 0 10 7 7 0 10 4 4 1 10 5 5 2 10 6 6 3 10 7 7 0 7 7 8'.split()))
-    #        alg = alg + [0]*(alglen - len(alg))
-            #[choice(actions) for i in range(alglen)]    
-#            alg = list(map(int, '0 11 10 4 1 11 10 5 2 11 10 6 3 11 10 7 0 4 10 4 1 5 10 5 2 6 10 6 3 7 10 7 8'.split()))
             alg = [0, 11, 10, 4, 1, 11, 10, 5, 2, 11, 10, 6, 3, 11, 10, 22, 0, 4, 10, 4, 1, 5, 10, 5, 2, 6, 10, 6, 3, 7, 10, 7, 8]
             alg = alg + [0]*(self.alglen - len(alg))
             self.pop_index = 0
-            #population = [[[choice(actions) for i in range(alglen)], 0] for j in range(pop_len//2)]+
             self.population = [[alg.copy(), 0] for j in range(self.pop_len)]
         if self.prevdeaths and deaths>self.prevdeaths:
             print('*', end='')
-            self.population[self.pop_index][1] = self.prevcycles#+prevhealth
+            self.population[self.pop_index][1] = self.prevcycles
             self.pop_index += 1
         if self.pop_index >= self.pop_len:
             self.pop_num += 1
@@ -89,7 +79,10 @@ class Behavior:
             save_rate = int(self.pop_len//self.mutation_rate)
             succ_pop = succ_pop[-save_rate:]
             succ_pop = list(map(lambda x: x[0], succ_pop))
-            self.population = succ_pop+[self.mutate(succ_pop[x%(save_rate)], self.actions, -x%save_rate+2, 3) for x in range(self.pop_len-save_rate)]
+            self.population = succ_pop+[self.mutate(succ_pop[x%(save_rate)], 
+                                                             self.actions, 
+                                                             -x%save_rate+2, 3) 
+    for x in range(self.pop_len-save_rate)]
             self.population = list(map(lambda x: [x]+[0], self.population))
             self.pop_index = 0
         alg = self.population[self.pop_index][0]
@@ -97,5 +90,4 @@ class Behavior:
         cur = self.parse(alg, cur, data)
         self.prevdeaths = deaths
         self.prevcycles = cycles
-#        return (1, 1)
         return cur
