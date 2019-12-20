68cf9a41daebe336061da5fbeda30ad0c1ee918e
asakhar <53300353+asakhar@users.noreply.github.com>
2019-12-20 18:03:14 +0300

    Update control_tests.py

--- control_tests.py
+++ control_tests.py
@@ -9,49 +9,53 @@ from control import Control
 from gameobject import Object
 
 class Control_Tests(unittest.TestCase):
-    
+
+    def setUp(self):
+        self.game = Control()
+        self.game += Object(type='custom')
+        self.game.addAutoSnake(scriptpath='template.py', color='red', name='testdel')
+        
     def test_1start(self):
-        global game
-        game = Control()
+        self.assertIsNotNone(self.game)
    
     def test_2add_object(self):
-        local_game = game
         object1 = Object(type='custom_type')
-        local_game += object1
-        self.assertIn(object1, game.getobjects())
+        self.game += object1
+        self.assertIn(object1, self.game.getobjects())
         
     def test_3getbyattr(self):
-        self.assertIsNotNone(game.getbyattr(attr='type', value='custom_type'))
+        self.assertIsNotNone(self.game.getbyattr(attr='type', value='custom'))
         
     def test_4add_auto_snake(self):
-        game.addAutoSnake(scriptpath='template.py', color='red', name='test')
-        autosnake = game.getbyattr(type='snake', attr='pn', value='autotest')
-        self.assertIn(autosnake, game.getobjects(type='snake'))
+        self.game.addAutoSnake(scriptpath='template.py', color='red', name='test')
+        autosnake = self.game.getbyattr(type='snake', attr='pn', value='autotest')
+        #self.assertIn(autosnake, game.getobjects(type='snake'))
         self.assertIsNotNone(autosnake)
     
     def test_5run1time(self):
-        game(run_once=True)
+        self.game(run_once=True)
     
     def test_6remove_object(self):
-        local_game = game
-        object1 = game.getbyattr(attr='type', value='custom_type')
-        self.assertIsNotNone(object1)
-        local_game -= object1
-        deleted_object1 = game.getbyattr(attr='type', value='custom_type')
+        object1 = self.game.getbyattr(attr='type', value='custom')
+        self.game -= object1
+        deleted_object1 = self.game.getbyattr(attr='type', value='custom')
         self.assertIsNone(deleted_object1)
         
     def test_7remove_autosnake(self):
-        local_game = game
-        autosnake = game.getbyattr(type='snake', attr='pn', value='autotest')
-        local_game -= autosnake
-        deleted_autosnake = game.getbyattr(type='snake', attr='pn', value='autotest')   
+        autosnake = self.game.getbyattr(type='snake', attr='pn', value='autotestdel')
+        self.game -= autosnake
+        deleted_autosnake = self.game.getbyattr(type='snake', attr='pn', value='autotestdel')   
         self.assertIsNone(deleted_autosnake)
     
-    def test_8run_one_more_time(self):
-        game(run_once=True)
+    def test_8run_two_times(self):
+        self.game(run_once=True)
+        self.game(run_once=True)
     
     def test_9close(self):
-        game.close()
-        
+        self.game.close()
+
+    def tearDown(self):
+        self.game.close()
+    
 if __name__ == '__main__':
     unittest.main()
